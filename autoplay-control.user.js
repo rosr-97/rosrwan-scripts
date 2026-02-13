aec805abfef9becdcf642ef51c8a8f851058fe35
rosrwan <61092374+rosr-97@users.noreply.github.com>
2026-02-13 14:55:08 -0600

    Add files via upload

---
+++ autoplay-control.user.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name         Autoplay Control on Playlists
+// @namespace    minawan
+// @version      2026-02-12
+// @description  Brings back the autoplay button into playlists.
+// @author       rosrwan
+// @match        https://*.youtube.com/*
+// @icon         https://icons.duckduckgo.com/ip2/youtube.com.ico
+// @grant        GM_addStyle
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  const css = `
+    .ytp-button.ytp-autonav-toggle.delhi-fast-follow-autonav-toggle {
+      display: inline-block !important;
+    }
+
+    .ytp-fullscreen-grid {
+      display: block !important;
+    }
+  `;
+
+  GM_addStyle(css);
+
+  let interval = undefined;
+  const ytdObserver = new MutationObserver((mutationsList, observer) => {
+    clearInterval(interval);
+
+    if ((/&list=/i.exec(location.search)?.length ?? 0) < 1) return;
+
+    const ytdPlayer = document.body?.querySelector('ytd-player');
+    if (!ytdPlayer) return;
+
+    const video = ytdPlayer.querySelector("video");
+    if (!video) return;
+
+    const autoplay = ytdPlayer.querySelector(".ytp-button.ytp-autonav-toggle.delhi-fast-follow-autonav-toggle .ytp-autonav-toggle-button");
+    if (!autoplay) return;
+
+    interval = setInterval(() => {
+      const duration = video.duration || 1;
+      const currentTime = video.currentTime || 0;
+      if ((duration - 0.6) > currentTime) return;
+
+      ytdPlayer.querySelector('.html5-video-player')?.classList.add('ytp-fullscreen-grid-active', 'ended-mode');
+      ytdPlayer.querySelector('.html5-video-player')?.classList.remove('paused-mode');
+      if (video.paused) return;
+
+      const isEnabled = autoplay.ariaChecked;
+      if (isEnabled === 'true') return;
+
+      const adReference = ytdPlayer.querySelector('.ytp-ad-player-overlay-layout__ad-info-container');
+      if (adReference) return;
+
+      video.pause();
+    }, 10);
+  });
+
+  new MutationObserver((mutationsList, observer) => {
+    for (const mutation of mutationsList)
+    {
+      if (!mutation.target?.classList.contains('ytd-player')) continue;
+      ytdObserver.observe(mutation.target, { childList:true, subtree:true });
+      observer.disconnect();
+    }
+  }).observe(document.body, { childList:true, subtree:true });
+})();
