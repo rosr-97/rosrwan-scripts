94876259b3a17950269930759f37f4e3bf98adfd
rosr-97 <vocaloid14@outlook.com>
2026-02-13 16:04:13 -0600

    fixes: performance

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
- 
+### My Scripts Repository 🍚
+
+[Greasy Fork - rosrwan](https://greasyfork.org/en/users/1570783-rosrwan)
--- autoplay-control.user.js
+++ autoplay-control.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Autoplay Control on Playlists
+// @name         Autoplay button on playlists
 // @namespace    minawan
-// @version      2026-02-12
-// @description  Brings back the autoplay button into playlists.
+// @version      2026-02-13
+// @description  Brings back the autoplay button into youtube playlists.
 // @author       rosrwan
 // @match        https://*.youtube.com/*
 // @icon         https://icons.duckduckgo.com/ip2/youtube.com.ico
@@ -22,38 +22,39 @@
       display: block !important;
     }
   `;
-
   GM_addStyle(css);
 
   let interval = undefined;
   const ytdObserver = new MutationObserver((mutationsList, observer) => {
     clearInterval(interval);
-
     if ((/&list=/i.exec(location.search)?.length ?? 0) < 1) return;
 
     const ytdPlayer = document.body?.querySelector('ytd-player');
     if (!ytdPlayer) return;
+    
+    const adReference = ytdPlayer.querySelector('.ytp-ad-player-overlay-layout__ad-info-container');
+    if (adReference) return;
 
     const video = ytdPlayer.querySelector("video");
     if (!video) return;
 
     const autoplay = ytdPlayer.querySelector(".ytp-button.ytp-autonav-toggle.delhi-fast-follow-autonav-toggle .ytp-autonav-toggle-button");
     if (!autoplay) return;
+    
+    const html5Player = ytdPlayer.querySelector('.html5-video-player');
+    if (!html5Player) return;
 
     interval = setInterval(() => {
       const duration = video.duration || 1;
       const currentTime = video.currentTime || 0;
       if ((duration - 0.6) > currentTime) return;
 
-      ytdPlayer.querySelector('.html5-video-player')?.classList.add('ytp-fullscreen-grid-active', 'ended-mode');
-      ytdPlayer.querySelector('.html5-video-player')?.classList.remove('paused-mode');
+      html5Player.classList.add('ytp-fullscreen-grid-active', 'ended-mode');
+      html5Player.classList.remove('paused-mode');
       if (video.paused) return;
 
       const isEnabled = autoplay.ariaChecked;
       if (isEnabled === 'true') return;
-
-      const adReference = ytdPlayer.querySelector('.ytp-ad-player-overlay-layout__ad-info-container');
-      if (adReference) return;
 
       video.pause();
     }, 10);
