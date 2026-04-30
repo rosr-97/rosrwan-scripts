449979a3811fdc34b43c9d14ba0db8925607475a
rosr-97 <vocaloid14@outlook.com>
2026-04-29 22:41:52 -0600

    changed: versioning

--- simple-minasona-badges.user.js
+++ simple-minasona-badges.user.js
@@ -2,7 +2,7 @@
 // @name         Simple Minasona Badges for FFZ
 // @namespace    https://github.com/rosr-97/rosrwan-scripts
 // @description  Simple implementation of the minasona badges for FrankerFacez.
-// @version      2026-29-04
+// @version      2026-30-04
 // @author       rosrwan
 // @match        https://www.twitch.tv/*
 // @icon         https://raw.githubusercontent.com/rosr-97/rosrwan-scripts/c5fd583eda27c2250aeebb305571b4727a069faf/assets/Minawan_Purple.png
